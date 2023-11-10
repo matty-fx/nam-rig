69d9419500e919c70e9bc592b65271e30e86b2eb
matty-fx <info@mattyfx.com>
2023-11-10 12:15:04 -0500

    Modify buffer types

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -8,7 +8,7 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::AudioAppComponent
+class MainComponent  : public juce::AudioAppComponent, private juce::Timer
 {
 public:
     //==============================================================================
@@ -23,10 +23,20 @@ public:
     //==============================================================================
     void paint (juce::Graphics& g) override;
     void resized() override;
+    void setCpuText(float cpu);
 
 private:
+    void timerCallback() override;
+    
     //==============================================================================
+    juce::AudioDeviceSelectorComponent audioSetupComp;
+    juce::Label cpuUsageText;
+    juce::Label cpuUsageLabel;
+    
     std::unique_ptr<DSP> model;
-
+    
+    double * inBuffer;
+    double * outBuffer;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
